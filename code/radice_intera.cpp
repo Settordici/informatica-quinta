89902ff646f827b4c73ca9ea74d3724532d4b9b3
Settordici <63635493+Settordici@users.noreply.github.com>
2022-10-08 16:00:48 +0200

    migliorata leggibilità e aggiunti commenti

--- code/radice_intera.cpp
+++ code/radice_intera.cpp
@@ -8,10 +8,10 @@ int main() {
     printf("CALCOLO DELLA RADICE CON APPROSSIMAZIONE INTERA\n");
     int num, i;
     printf("Inserire il numero di partenza: ");
-    scanf("%d", &num);
+    scanf("%d", &num); //Puntatore a num per evitare un segmentation fault
 
     while(i*i<num) {
-        if(((i+1)*(i+1)-num) > (num-i*i)) {
+        if((i+1)*(i+1)-num > num-i*i) { //Controlla la differenza tra num e i due valori di i, i e i+1, per determinare l'approssimazione finale
             break;
         }
 
@@ -19,5 +19,6 @@ int main() {
     }
 
     printf("La radice di %d e': %d", num, i);
-    return 0;
+
+    return 0; //Esecuzione con successo
 }
